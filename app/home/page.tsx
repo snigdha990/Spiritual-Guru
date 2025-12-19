ca6bdcf5077f12aa016d1455959158e1301d2bbe
snigdha990 <saisnigdha1811@gmail.com>
2025-12-19 22:48:12 +0530

    changed home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,9 +3,7 @@ import HomePageClient from "./HomePageClient";
 
 export default async function Page({ searchParams }: { searchParams: any }) {
   const params = await searchParams;
-
-  const lang = params.lang === "Hindi" ? "Hindi" : "English";
   const guru = params.guru || "Sai Baba";
 
-  return <HomePageClient lang={lang} guru={guru} />;
+  return <HomePageClient  guru={guru} />;
 }
